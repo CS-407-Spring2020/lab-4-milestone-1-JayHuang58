cc26ab713210f82e4edb0a0fa2126f641073afcc
jayhuang <shuang362@wisc.edu>
2020-02-21 21:27:14 -0600

    add Non-UI thread

--- app/src/main/java/com/example/lab4milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab4milestone1/MainActivity.java
@@ -3,12 +3,84 @@ package com.example.lab4milestone1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private Button buttonStartThread;
+    // volatile to make sure this change is instantaneously to all threads
+    private volatile boolean stopThread = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonStartThread = findViewById(R.id.button_start_thread);
     }
+
+    public void startThread(View view){
+        stopThread = false;
+        ExampleRunnable runnable = new ExampleRunnable(10);
+        new Thread(runnable).start();
+
+    }
+
+
+    public void stopThread(View view){
+        stopThread = true;
+    }
+
+
+
+    class ExampleRunnable implements Runnable {
+        int seconds;
+        ExampleRunnable(int seconds) {
+            this.seconds = seconds;
+        }
+        @Override
+        public void run(){
+            for (int i = 0; i<seconds; i++){
+                // when resume thread or never add new thread,
+                if(stopThread) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            buttonStartThread.setText("Start");
+                        }
+                    });
+                    return;
+                }
+
+                if (i == 5) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            buttonStartThread.setText("50%");
+                        }
+                    });
+                }
+
+                Log.d(TAG,"startThread: "+i);
+                try{
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    buttonStartThread.setText("Start");
+                }
+            });
+        }
+    } // end of ExampleRunnable class
+
 }
+
+
